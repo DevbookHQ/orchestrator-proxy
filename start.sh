3833d1c5442ec689c9b2426cb82419592b69c365
Vaclav Mlejnsky <vasek.mlejnsky@gmail.com>
2021-10-06 14:20:15 +0000

    Add start script

---
+++ start.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+set -euxo pipefail
+
+docker-compose() {
+  docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose:1.29.2 "$@"
+}
+
+docker-compose up
